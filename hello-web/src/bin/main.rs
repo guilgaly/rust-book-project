08f44b1ed6e99c46eec0e297e3e0ec49e2f1d7ff
Guillaume Galy <guilgaly@gmail.com>
2020-04-04 16:47:37 +0200

    Hello web app 2

--- hello-web/src/bin/main.rs
+++ hello-web/src/bin/main.rs
@@ -1,13 +1,19 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::prelude::*;
+use hello_web::ThreadPool;
 use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -16,7 +22,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get_root = b"GET / HTTP/1.1\r\n";
+    let get_sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, contents_path) = if buffer.starts_with(get_root) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(get_sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
